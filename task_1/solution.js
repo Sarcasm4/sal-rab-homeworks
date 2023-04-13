ebaee309c3e08440bd472ecf29a0dac2730e730e
Danya <popovd94@gmail.com>
2023-04-13 13:43:58 +0400

    task 1 real done

--- task_1/solution.js
+++ task_1/solution.js
@@ -4,16 +4,16 @@ function addInCartMessage(title, price) {
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
-    // создайте переменную message
-    // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    // выведите переменную message в консоль
-    // комментарий
+    let message;
+    message = productName;
+    message = message + " "
+    message = message + "за"
+    message = message + " "
+    message = message + productPrice
+    message = message + " "
+    message = message + "теперь в корзине!"
+    console.log(message);
+    
     // Конец решения задания №1.1.
 
     return message;
@@ -23,10 +23,9 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
-
-    // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
+    let newValue;
+    newValue = oldValue;
+    newValue = oldValue +  1;
 
     // Конец решения задания №1.2.
 
@@ -38,10 +37,11 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
-
-    // создайте переменную newSum и присвойте ей сумму oldSum и difference
-    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
+    let newSum;
+    newSum = oldSum + difference;
+    let newSumText;
+    newSumText = newSum + " ₽"
+    newSumText = `${newSum} ₽`;
 
     // Конец решения задания №1.3.
 
